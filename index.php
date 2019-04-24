751c23a0cd2bb0f44d89edf146ce75b2a709c6a4
Pomogranets <44092082+Pomogranets@users.noreply.github.com>
2019-04-24 16:04:06 +0200

    Update index.php
    test

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
 <!-- This is your mandatory theme template file. It normally contains a combination of WP function calls (to insert content from the database) and HTML to structure the content -->
+<p> Hi </p>

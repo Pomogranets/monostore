2c5b51f681d5a644790e523e921d348d68409a90
Pomogranets <44092082+Pomogranets@users.noreply.github.com>
2019-05-03 14:35:25 +0200

    Created empty files to work in
    Created empty files to work in

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!-- This is your mandatory theme template file. It normally contains a combination of WP function calls (to insert content from the database) and HTML to structure the content -->
+
 <p> Hi, I'm here </p>
 <p> Are you there? </p>
 <p> MUAHAH </p>

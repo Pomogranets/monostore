a91bd836df147cdef99416f89463253a8c837655
aliciawikdahl <alicia98@me.com>
2019-05-05 16:59:49 +0200

    Some small work on frontpage and 404 page
    Started setting up the base for the pages. Added some text to 404 and some php to front-page.

--- 404.php
+++ 404.php
@@ -6,8 +6,11 @@
 	<title>TEST</title>
 </head>
 <body>
+	<h1 class="404-header">404</h1>
 	<p>
-		TEST!!!!!!
+		Tyvärr hittade vi inte den sidan du sökte efter. Du kan antinge gå tillbaka till sidan du kom ifrån, gå till vår startsida eller passa på att kolla in vår fina webbshop!
 	</p>
+	<button>Startsida</button>
+	<button>Webbshop</button>
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -5,5 +5,19 @@
 		
 	<!-- SLIDER PLUGIN ?? -->
 
+	<?php
+	//Welcome text
+		echo '<h1>';
+			the_title();
+		echo '</h1>';
+
+	//Content/Headers/Images
+		echo '<div class="front-page-content">';				the_post();
+
+			the_content();
+		echo '</div>';
+		
+		?>
+
 	</main>
 <?php get_footer(); ?>

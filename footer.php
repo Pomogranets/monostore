523e3ca456136da1929682236069768f279ccf53
Emily <olem1794@student.ju.se>
2019-05-06 20:23:08 +0200

    Started on the menu and footer

---
+++ footer.php
@@ -0,0 +1,15 @@
+</div>  <!-- end site-container -->
+
+    <footer>
+
+      <p class="copyright">©All Rights Reserved.</p>
+
+      <div class="social-icons">
+        <a href=""><i class="fab fa-instagram icon"></i></a>
+        <a href=""><i class="fab fa-facebook-square icon"></i></a>
+      </div>
+
+    </footer>
+<?php wp_footer(); ?>
+  </body>
+</html>

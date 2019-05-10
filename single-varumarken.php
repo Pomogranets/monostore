214e85c21b59f24f07d8ed59935a423e341a5573
Emily <olem1794@student.ju.se>
2019-05-10 16:56:55 +0200

    test2

--- single-varumarken.php
+++ single-varumarken.php
@@ -1,16 +1,17 @@
 <?php get_header();
 
 if( have_posts() ) {
-echo "yes we have post";
-    // while ( have_posts() ){
-    //     the_post();
-    //
-    //     the_post_thumbnail():
-    //
-    //     the_title();
-    //
-    //     the_content();
-    //   }
+
+    while ( have_posts() ){
+
+        the_post();
+    echo "yes we have post";
+        // the_post_thumbnail():
+        //
+        // the_title();
+        //
+        // the_content();
+      }
     }
 
 

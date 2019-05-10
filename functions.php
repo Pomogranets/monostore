dc48ab54674167264541ea9818df58d3df5f191b
Emily <olem1794@student.ju.se>
2019-05-10 14:42:05 +0200

    Fixing issue Attempt #1

--- functions.php
+++ functions.php
@@ -126,11 +126,11 @@ function change_logo_class($html) {
 }
 
 //add google fonts
-function add_google_fonts() {
-      wp_enqueue_style( 'custom-google-fonts', '<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700|Raleway:400,700,800" rel="stylesheet">', false );
-}
-
-add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
+// function add_google_fonts() {
+//       wp_enqueue_style( 'custom-google-fonts', '<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700|Raleway:400,700,800" rel="stylesheet">', false );
+// }
+//
+// add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
 
 
 //Register our sidebars and widgetized areas.

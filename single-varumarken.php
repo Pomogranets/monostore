59babd1a52a3ae6085b2750a30c0dc5cebeea185
Emily <olem1794@student.ju.se>
2019-05-10 16:53:15 +0200

    change the file

--- single-varumarken.php
+++ single-varumarken.php
@@ -1,17 +1,17 @@
 <?php get_header();
 
-if( have_posts() ) {
-
-    while ( have_posts() ){
-        the_post();
-
-        the_post_thumbnail():
-        
-        the_title();
-
-        the_content();
-      }
-    }
+// if( have_posts() ) {
+//
+//     while ( have_posts() ){
+//         the_post();
+//
+//         the_post_thumbnail():
+//
+//         the_title();
+//
+//         the_content();
+//       }
+//     }
 
 
 get_footer();

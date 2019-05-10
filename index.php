8a180b4bf64f1b57382980da3b0ec3cf9df0837b
Pomogranets <44092082+Pomogranets@users.noreply.github.com>
2019-05-10 16:19:09 +0200

    Fixed index and worked on the kontakt page
    Email does not work, Server issue?

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <!-- This is your mandatory theme template file. It normally contains a combination of WP function calls (to insert content from the database) and HTML to structure the content -->
 
-<p> Hi, I'm here </p>
-<p> Are you there? </p>
-<p> MUAHAH </p>
+<?php /* Template Name: Contact-page */
+get_header();
+
+echo '<div id="page">';
+
+if(have_posts()) {
+
+    while ( have_posts() ){
+
+    	echo '<div class="contact-content">';
+
+    	the_post();
+    	
+    	the_title('<h1>', '</h1><br>');
+
+    	the_content('<p>', '</p>');
+
+        echo '</div>';
+    }
+
+} else {
+	echo 'Informationen saknas för tillfället.';
+}
+
+	wp_reset_postdata();
+	get_footer(); 
+?>

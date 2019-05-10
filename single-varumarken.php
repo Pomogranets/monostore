dab9a3a074385c955f1929210930fc32e8bbdad0
Emily <olem1794@student.ju.se>
2019-05-10 17:03:42 +0200

    test6

--- single-varumarken.php
+++ single-varumarken.php
@@ -5,9 +5,10 @@ if( have_posts() ) {
     while ( have_posts() ){
 
         the_post();
-    echo "yes we have many many post";
+    echo "yeah many posts";
 
-      
+    the_post_thumbnail('small_thumbnail',  array( 'class' => 'about-image' ));
+
         the_title();
 
         the_content();

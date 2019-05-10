a7ee97d8ef807ad4b1148102bb5702f27b223770
Emily <olem1794@student.ju.se>
2019-05-10 16:59:22 +0200

    test4

--- single-varumarken.php
+++ single-varumarken.php
@@ -5,10 +5,10 @@ if( have_posts() ) {
     while ( have_posts() ){
 
         the_post();
-    echo "yes we have many post";
-        // the_post_thumbnail():
+    echo "yes we have many many post";
+        the_post_thumbnail('small_thumbnail',  array( 'class' => 'varu-image' )):
         //
-        // the_title();
+        the_title();
         //
         // the_content();
       }

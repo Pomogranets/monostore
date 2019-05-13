0f9bfd3c0de81fb62241fd885d592b548db6c374
Emily <olem1794@student.ju.se>
2019-05-13 15:41:46 +0200

    trying to show all the fields

--- page-art.php
+++ page-art.php
@@ -39,7 +39,7 @@ if ( $query->have_posts() ) {
     <div class="grid-container">
 
           <figure class="art-thumbnail"> <?php the_post_thumbnail('single_art',  array( 'class' => 'art-grid' )); ?> </figure>
-          <figcaption class="art-title"> <?php the_title() .' | '. the_title() ; ?>  </figcaption>
+          <figcaption class="art-title"> <?php the_title()." | ".the_title() ; ?>  </figcaption>
 
       </div>
 

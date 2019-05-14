0f1d9d39751b52a1c7e7b1f0a7737e47a93a810f
Pomogranets <44092082+Pomogranets@users.noreply.github.com>
2019-05-14 15:53:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- This is your mandatory theme template file. It normally contains a combination of WP function calls (to insert content from the database) and HTML to structure the content -->
 
-<?php /* Template Name: Contact-page */
+<?php /* Template Name: Index */
 get_header();
 
 echo '<div id="page">';

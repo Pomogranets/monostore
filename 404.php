a7bf1d06b997dd15ed5cfedfc7ae5fa984237f2c
aliciawikdahl <alicia98@me.com>
2019-05-24 16:01:03 +0200

    Noticed an error in 404 page

--- 404.php
+++ 404.php
@@ -1,28 +1,17 @@
 <!-- 404 PAGE -->
 
 
-<?php get_header(); /* Template Name: 404-page */ ?>
+<?php get_header(); ?>
 
 	<main class="error-page-body">
 		<h1 class="error-header">404</h1>
 		
-		<?php
-		
-		echo '<div class="error-page-content">';
-		
-				the_post();
-
-				the_content();
-		
-		echo '</div>';
-		
-		?>
-		
-		<!-- <p>
+		<p>
 			Tyvärr hittade vi inte den sidan du sökte efter. Du kan antingen gå tillbaka till sidan du kom ifrån, gå till vår startsida eller passa på att kolla in vår fina webbshop!
-		</p> -->
+		</p>
+		
 		<a href=" <?php get_site_url();?> / "><button class="error-btn" type="button" name="button">Startsida</button></a>
-		<a href=" <?php get_site_url();?> ./shop/"><button class="error-btn" type="button" name="button">Webbshop</button></a>
+		<a href=" <?php get_site_url();?> ./webbshop/"><button class="error-btn" type="button" name="button">Webbshop</button></a>
 	</main>
 
 <?php get_footer(); ?>
